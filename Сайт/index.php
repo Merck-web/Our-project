3d4f12c432ddfe3b3d89e0f9a68f96c7753d67cb
mrclll <alekseyt.ognevoy@mail.ru>
2020-12-18 14:21:26 +0500

    Сайт с нуля

---
+++ Сайт/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+      <div id="logo">
+        <a href="/" title="Перейти на главну.">
+          <span>W</span>ild<span>T</span>agil
+        </a>
+      </div>
+      <div id="menuHead">
+<a href="/about.php">
+  <div style="margin-right: 5%"> О нас </div>
+</a>
+<a href="/feedback.php">
+  <div>Обратная связь</div>
+  </a>
+      </div>
+      <div id="regAuth">
+<a href="/reg.php">Регистрация</a> |  <a href="/auth.php">Авторизация</a>
+      </div>
+    </header>
+    <div id="wrapper">
+      <div id="leftCol">
+<div id="bigArticle">
+
+</div>
+<div class="clear"></div>
+<div id="article">
+<img src="img/article1.jpg" alt="Статья 1" title="Статья 1">
+<h2>Статья 1</h2>
+<p>Сеть зоомагазинов &laquo;Природа&raquo; представляет полный спектр зоотоваров. В&nbsp;наших зоомагазинах &laquo;Природа&raquo; большой выбор кормов ведущих производителей: Hill`s (Хилл`с), Royal Canin (Роял Канин), Eukanuba (Эукануба), Iams (Иамс), Про План (ProPlan), Бош (Bosch), Happy Dog (Хеппи Дог), Хэппи Кэт (Happy Cat), Whiskas (Вискас), Kitekat (КитеКэт), Pedigree (Педигри), Chappi (Чаппи), Diamond (Даймонд), Нутра Голд (Nutra Gold), а&nbsp;также многих других кормов от&nbsp;эконом- до&nbsp;суперпремиум класса, в&nbsp;том числе лечебных и&nbsp;диетических. Также в&nbsp;наших зоомагазинах &laquo;Природа&raquo; Вы&nbsp;найдете большой выбор других зоотоваров: пищевые добавки, ветеринарные препараты, предметы ухода за&nbsp;животными. У&nbsp;нас можно приобрести различные лакомства и&nbsp;специализированные корма и&nbsp;подкормки. Любой зоомагазин &laquo;Природа&raquo; представляет покупателям богатый выбор зоотоваров и&nbsp;кормов для птиц, грызунов, черепах и&nbsp;рыб.
+                    В&nbsp;каждом зоомагазине &laquo;Природа&raquo; продаются не&nbsp;только корма, но&nbsp;и&nbsp;аксессуары: клетки, миски, переноски, игрушки, когтеточки, одежда для животных, поводки, ошейники, домики, наполнители для туалетов, тримминги, щетки, когтерезы, пуходерки, лотки, лежаки, спреи, шампуни, дезодоранты, лосьоны, кремы, бальзамы, пудры, увлажнители и&nbsp;многое-многое другое.
+</p>
+<div>Далее</div>
+</div>
+      </div>
+      <div id="rightCol">
+<div class="banner">
+  <img src="/img/baner1.jpg" alt="Баннер 1" title="<Баннер 1">
+</div>
+<div class="banner">
+  <img src="/img/baner2.jpg" alt="Баннер 1" title="<Баннер 1">
+</div>
+      </div>
+
+    </div>
+<footer>
+<div id="social">
+  <a href="https://vk.com/rhymeru" title="Группа в Вк" target="_blank"</a>
+    <img src="img/Vk.png" alt"Вк" title="Вк"></a>
+  <a href="https://www.facebook.com/" title="Группа в Facebook" target="_blank"</a>
+    <img src="img/Facebook.png" alt"Facebook" title="Facebook"></a>
+  <a href="https://www.instagram.com/" title="Группа в Instagram" target="_blank"</a>
+    <img src="img/Instagram.png" alt"Instagram" title="Instagram"></a>
+
+  </div>
+  <div id="rights">
+    Все права защищены@  <?php echo date ('Y') ?>
+    </div>
+</footer>
+</body>
+
+</html>
