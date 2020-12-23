d7a3943f8a7459ea0c995275ea2d83ea8d753b32
mrclll <alekseyt.ognevoy@mail.ru>
2020-12-23 17:03:16 +0500

    123

---
+++ Сайт/blocks/rightCol.php
@@ -0,0 +1,8 @@
+<div id="rightCol">
+<div class="banner">
+<img src="/img/baner1.jpg" alt="Баннер 1" title="<Баннер 1">
+</div>
+<div class="banner">
+<img src="/img/baner2.jpg" alt="Баннер 1" title="<Баннер 1">
+</div>
+</div>
--- Сайт/feedback.php
+++ Сайт/feedback.php
@@ -1,30 +1,16 @@
 <!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <header>
-      <div id="logo">
-        <a href="/" title="Перейти на главну.">
-          <span>W</span>ild<span>T</span>agil
-        </a>
+<html>
+  <head>
+      <?php
+$title = "Обратная связь";
+require_once "blocks/head.php";
+?>
+  </head>
+  <body>
+      <?php require_once "blocks/header.php" ?>
+      <div id="wrapper">
+        <?php require_once "blocks/form.php" ?>
       </div>
-      <div id="menuHead">
-<a href="/about.php">
-  <div style="margin-right: 5%"> О нас </div>
-</a>
-<a href="/feedback.php">
-  <div>Обратная связь</div>
-  </a>
-      </div>
-      <div id="regAuth">
-<a href="/reg.php">Регистрация</a> |  <a href="/auth.php">Авторизация</a>
-      </div>
-    </header>
-</body>
-
+      <?php require_once "blocks/footer.php" ?>
+  </body>
 </html>
